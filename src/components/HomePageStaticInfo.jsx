098bc66ef068d4bbd8fd8f4f5ee2f218429a7195
rrjafarov <rafael.cefer.4050@gmail.com>
2025-03-30 15:03:05 +0400

    Fix import path for PartByPartRed icon in HomePageStaticInfo component

--- src/components/HomePageStaticInfo.jsx
+++ src/components/HomePageStaticInfo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import DeliveryRed from "../../public/icons/deliveryRed.svg";
 import DeliveryBlack from "../../public/icons/deliveryBlack.svg";
 import OfficialRed from "../../public/icons/officialRed.svg";
-// import PartByPartRed from "../../public/icons/partByPartRed.svg";
+import PartByPartRed from "../../public/icons/partBypartRed.svg";
 import OperatorRed from "../../public/icons/operatorsRed.svg";
 
 const HomePageStaticInfo = () => {
@@ -36,12 +36,12 @@ const HomePageStaticInfo = () => {
             <div className="xl-3 lg-3 md-6 sm-6">
               <div className="staticInfoCard">
                 <div className="staticInfoImage">
-                  {/* <PartByPartRed className="deliveryIcon" /> */}
-                  <img
+                  <PartByPartRed className="deliveryIcon" />
+                  {/* <img
                     className="deliveryIcon"
-                    src="/icons/partByPartRed.svg"
+                    src="/icons/partBypartRed.svg"
                     alt="dee"
-                  />
+                  /> */}
                 </div>
                 <div className="staticInfoContent">
                   <span>Hissə-hissə ödəniş</span>
