206ea2c8a47c89ec7cf5d951e95b7d1d06f098ce
rrjafarov <rafael.cefer.4050@gmail.com>
2025-07-24 11:55:51 +0400

    wd

--- next.config.mjs
+++ next.config.mjs
@@ -1,65 +1,3 @@
-// import createNextIntlPlugin from "next-intl/plugin";
-
-// const withNextIntl = createNextIntlPlugin({
-//   locales: ["az", "en", "ru"],
-//   defaultLocale: "az",
-// });
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         // protocol: "https",
-//         protocol: "https",
-//         // hostname: "//",
-//         hostname: "dev-admin.barkodelectronics.az",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "img.youtube.com",
-//       },
-//     ],
-//   },
-
-//   // output: "export",
-
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: [
-//         {
-//           loader: "@svgr/webpack",
-//           options: {
-//             svgoConfig: {
-//               plugins: [
-//                 {
-//                   name: "preset-default",
-//                   params: {
-//                     overrides: {
-//                       removeViewBox: false,
-//                     },
-//                   },
-//                 },
-//               ],
-//             },
-//           },
-//         },
-//       ],
-//     });
-
-//     return config;
-//   },
-// };
-
-// export default withNextIntl(nextConfig);
-
-
-
-
-
-
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin({
@@ -72,19 +10,20 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
+        // protocol: "https",
+        protocol: "https//",
+        // hostname: "//",
         hostname: "dev-admin.barkodelectronics.az",
-        port: "",
         pathname: "/**",
       },
       {
-        protocol: "https",
+        protocol: "https//",
         hostname: "img.youtube.com",
-        port: "",
-        pathname: "/**",
       },
     ],
   },
+
+  // output: "export",
 
   webpack(config) {
     config.module.rules.push({
@@ -115,7 +54,4 @@ const nextConfig = {
 };
 
 export default withNextIntl(nextConfig);
-
-
-
 
